65ff71d83e1034c8a89c268b9ae577e7eb5c84c8
Igor-S-M <igorsousa3148@gmail.com>
2023-01-11 00:51:57 -0300

    fix: get orders and order by id response

--- src/controller/orders.controller/orders.controller.js
+++ src/controller/orders.controller/orders.controller.js
@@ -31,7 +31,9 @@ export async function getOrder(req, res) {
       return res.status(404).send("não tem pedido");
     }
 
-    res.status(200).send(ordersConsult.rows);
+    const orders = ordersConsult.rows.map(i=>organizeOrder(i))
+
+    res.status(200).send(orders);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
@@ -42,7 +44,7 @@ export async function getOrderById(req, res) {
   const { id } = req.params;
 
   try {
-    const ordersConsult = await connection.query(
+    const orderConsult = await connection.query(
       `SELECT 
             o.id as "orderId", o.quantity, o."createdAt", o."totalPrice",
             o."clientId",cl.name as "clientName", cl.address,cl.phone,
@@ -53,9 +55,37 @@ export async function getOrderById(req, res) {
             WHERE o.id = $1;`,
       [id]
     );
-    res.status(200).send(ordersConsult.rows)
+
+    const order = organizeOrder(orderConsult.rows[0]) 
+
+    res.status(200).send(order);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
   }
+}
+
+function organizeOrder(obj) {
+
+  const body = {
+    client: {
+      id: obj.clientId,
+      name: obj.clientName,
+      address: obj.address,
+      phone: obj.phone,
+    },
+    cake: {
+      id: obj.cakeId,
+      name: obj.cakeName,
+      price: obj.price,
+      description: obj.description,
+      image: obj.image,
+    },
+    orderId: obj.orderId,
+    createdAt: obj.createdAt,
+    quantity: obj.quantity,
+    totalPrice: obj.totalPrice,
+  };
+
+  return body
 }
